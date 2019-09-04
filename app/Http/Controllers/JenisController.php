ad43dab6dcd8304b0b62bfa53c7491d0d00956b2
SetiawanRizals-oss <rizalsetiawan292@gmail.com>
2019-09-04 17:29:24 +0700

    baru nih

--- app/Http/Controllers/JenisController.php
+++ app/Http/Controllers/JenisController.php
@@ -1,10 +1,179 @@
 <?php
 
 namespace App\Http\Controllers;
-use\App\jenis
+use\App\jenis;
 use Illuminate\Http\Request;
 
 class JenisController extends Controller
 {
-    //
+      public function index()
+    {
+		if(request()->ajax()){
+			return datatables()->of(jenis::latest()->get())
+			//latestnya diganti where('is_delete','=',1)->get())
+			
+             ->addColumn('action',function($data){
+			if($data->isDelete==0){
+				$button = '<button type="button"
+			    name="action" id="'.$data->kodeJenis.'"
+			    class="btn btn-outline-dark
+			    btn-sm">Non Aktif</button>';
+			    $button .="&nbsp;&nbsp;&nbsp;" ;
+			    $button .= '<button type="button"
+			    name="action" id="'.$data->kodeJenis.'"
+			    class="darkness btn btn-success
+			    btn-sm">Aktifkan</button>';
+			    return $button;
+			}
+			else{
+				 $button = '<button type="button"
+			    name="action" id="'.$data->kodeJenis.'"
+			    class="btn btn-success
+			    btn-sm">Aktif</button>';
+			    return $button;
+			}
+
+				
+           })
+			
+            ->addColumn('detail',function($data){
+			$button = '<button type="button"
+			name="detail" id="'.$data->kodeJenis.'"
+			class="detail btn btn-secondary
+			btn-sm">Lihat</button>';
+			return $button;
+			})
+
+
+
+
+			->addColumn('edit',function($data){
+				
+         
+				$button ='<button type="button"
+				name="edit" id="' .$data->kodeJenis.'"
+				class="edit btn btn-primary
+				btn-sm">Ubah</button>';
+			return $button;
+			})
+			
+
+			->addColumn('delete',function($data){
+			$button = '<button type="button"
+			name="delete" id="'.$data->kodeJenis.'"
+			class="delete btn btn-danger
+			btn-sm">Hapus</button>';
+			return $button;
+			})
+
+          
+        
+			
+     
+
+
+			 
+        
+			->rawColumns(array("action","detail","edit","delete"))
+			->make(true);
+			return view('Jenis.index_jenis');
+			
+			
+			}
+
+    }
+
+
+public function add(Request $request){ //post udah pasti  $request kalo ngelempar data pakai form berarti pake $request
+    $isDelete = 1;
+
+
+
+    $form_data =array(
+          'kodeJenis'=>$request->kodeJenis,
+          'namaJenis'=>$request->namaJenis,
+          'promoJenis'=>$request->promoJenis,
+          'isDelete'=>$isDelete
+          
+    );
+    $kodeJen = jenis::where('kodeJenis', '=' , $request->kodeJenis)->get(); //nyari data
+    $count=count($kodeJen);
+
+    if($count==1){
+      return response()->json(['error'=>' Data telah ada']);
+    }
+    else{
+     jenis::create($form_data);
+     return response()->json(['success'=>'Data Berhasil Ditambah']);
+    }
+
+    /*var_dump($kodePel->kode_pelajaran);*/
+
+     
+
+    }
+
+
+
+ public function detail(Request $request, $kodeJenis)
+    {
+        if($request->ajax()){
+			$data =jenis::where('kodeJenis', '=', $kodeJenis)->get();
+			return response()->json(['data'=>$data]);
+		} //elorquen
+    }
+
+
+public function edit(Request $request, $kodeJenis)
+    {
+        if($request->ajax()){
+			$data =jenis::where('kodeJenis', '=', $kodeJenis)->get();
+			return response()->json(['data'=>$data]);
+		} //elorquen
+    }
+
+public function update(Request $request)
+  {
+  	 $isDelete = 1;
+   $form_data =array(
+          
+          'kodeJenis'=>$request->kodeJenis,
+          'namaJenis'=>$request->namaJenis,
+          'promoJenis'=>$request->promoJenis
+       
+          
+    );
+   
+    $kodeJen = jenis::where('kodeJenis', '=' , $request->kodeJenis)->update($form_data); //nyari data
+        return response()->json(['success'=>' Nah dah ke update tuh']);
+ }
+
+
+ public function destroy($kodeJenis)
+      {
+       
+         $isDelete = 1;	
+         $form_data =array(
+          'isDelete'=>0
+         );
+   
+    $kodeJen = jenis::where('kodeJenis',$kodeJenis)->update($form_data); //nyari data
+        return response()->json(['success'=>' Nah dah ke update tuh']);
+ }
+
+
+public function dark($kodeJenis)
+      {
+       
+         $isDelete = 0;	
+         $form_data =array(
+          'isDelete'=>1
+         );
+   
+    $kodeJen = jenis::where('kodeJenis',$kodeJenis)->update($form_data); //nyari data
+        return response()->json(['success'=>' Nah dah balik tuh']);
+ }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('Jenis.index_jenis');
 });
+Route::get('/jenis','JenisController@index');
+Route::post('/jenis/add','JenisController@add');
+Route::get('jenis/detail/{id}','JenisController@detail');
+Route::get('jenis/edit/{id}','JenisController@edit');
+Route::post('/jenis/update','JenisController@update');
+Route::get('/jenis/destroy/{lets}','JenisController@destroy');
+Route::get('/jenis/active/{is_darkness}','JenisController@dark');
