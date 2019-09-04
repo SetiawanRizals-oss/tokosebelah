27d283329bbb8fe4b671fd0beb997d8765292fd6
brianokawaganteng <brianokawa10@gmail.com>
2019-09-04 11:04:23 +0700

    Brian Migrations

---
+++ database/migrations/2019_09_04_033947_create_toko_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTokoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('toko', function (Blueprint $table) {
+            $table->string('kodeToko',5);
+            $table->string('namaToko',20);
+            $table->integer('ratingToko');
+            $table->integer('isDelete');
+            $table->date('tanggalHapus');
+            $table->primary('kodeToko');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('toko');
+    }
+}
